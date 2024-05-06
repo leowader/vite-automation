3e0a101dd352e73d95beae94e8127712fc4b98eb
leowader <waderhe@gmail.com>
2024-05-06 16:20:53 -0500

    creando carpetas dentro del proyecto generado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { spawn } = require("child_process");
-const projectName = "mi_proyecto_vite";
-const directory = "probando"; 
+const { spawn, exec } = require("child_process");
+const fs = require("fs");
+const projectName = "lovite";
+const directory = "probando";
 const comando = `npm create vite@latest ${projectName}`;
 
 // Ejecutar el comando
@@ -8,8 +9,8 @@ const proceso = spawn(comando, { cwd: directory, shell: true });
 let seleccionado = false;
 proceso.stdout.on("data", (data) => {
   const salida = data.toString();
-  process.stdout.write(salida); // Imprimir la salida en la consola
-  if (salida.includes("Select a framework:") && seleccionado===false) {
+  // process.stdout.write(salida); // Imprimir la salida en la consola
+  if (salida.includes("Select a framework:") && seleccionado === false) {
     proceso.stdin.write("\x1B[B\x1B[B\n");
     seleccionado = true; // Enviar teclas de flecha hacia abajo y Enter para seleccionar 'React'
   }
@@ -17,12 +18,25 @@ proceso.stdout.on("data", (data) => {
     proceso.stdin.write("x1B[B\n"); // Enviar teclas de flecha hacia abajo y Enter para seleccionar 'TypeScript'
   }
 });
-proceso.on("close", (codigo) => {
-  if (codigo === 0) {
-    console.log(
-      `Proyecto Vite con React creado correctamente en ${directory}/${projectName}.`
-    );
-  } else {
-    console.error(`Error al crear el proyecto Vite: ${codigo}`);
-  }
-});
+proceso.on("close", (code) => {});
+setTimeout(() => {
+  createStructure();
+}, 5000);
+function createStructure() {
+  const lovitePath = `${directory}/${projectName}`;
+  fs.access(lovitePath, fs.constants.F_OK, (err) => {
+    if (!err) {
+      console.log("lov", lovitePath);
+      const nombreCarpeta = "context";
+      const ruta = lovitePath + `/src/${nombreCarpeta}`;
+      if (!fs.existsSync(ruta)) {
+        fs.mkdirSync(ruta);
+        console.log("¡Carpeta creada exitosamente!");
+      } else {
+        console.log("La carpeta ya existe.");
+      }
+    } else {
+      console.error(`Error: No se encontró el directorio ${lovitePath}`);
+    }
+  });
+}
